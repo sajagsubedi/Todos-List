9f6e47a8a41cdb88025c6843830b9a9c45849ef2
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2022-11-20 17:48:43 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+Uuimport { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+base:'/Todo-s-List/',
   server: {
     host: '0.0.0.0',
   }
